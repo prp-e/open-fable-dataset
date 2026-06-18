9674191196b67d3fbdb9234e0553fec29269b05d
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-06-18 22:14:57 +0330

    added boilerplates

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ from openai import OpenAI
 import sqlite3
 
 client = OpenAI(base_url=OPENAI_ENDPOINT, api_key=OPENAI_API_KEY)
+
+def generate_question(category):
+    pass
+
+def generate_answer(question):
+    pass
+
+def generate_value(question, answer, category):
+    pass
