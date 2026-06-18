b0fb09246644def4782d671afb00b75d0b9d391f
Muhammadreza Haghiri <haghiri75@gmail.com>
2026-06-18 22:19:31 +0330

    wip

--- main.py
+++ main.py
@@ -5,7 +5,21 @@ import sqlite3
 client = OpenAI(base_url=OPENAI_ENDPOINT, api_key=OPENAI_API_KEY)
 
 def generate_question(category):
-    pass
+    response = client.chat.completions(
+        model = MODEL,
+        messages = [
+            {
+                "role" : "system",
+                "content" : SYSTEM_PROMPT
+            },
+            {
+                "role" : "user",
+                "content" : f"Generate a master's degree problem in the field of {category}"
+            }
+        ]
+    )
+
+    return response
 
 def generate_answer(question):
     pass
